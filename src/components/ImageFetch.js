42e2b0edcd6909277c941b4714050a6dc0f99d41
Artie <turo310@gmail.com>
2019-11-27 21:54:30 -0800

    Include env file for keys

--- src/components/ImageFetch.js
+++ src/components/ImageFetch.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+
+const API_KEY = process.env.REACT_APP_NASA_API;
+
 const ImageFetch = () => {
     const [daily, setDaily] = useState([]);
 
     async function fetchData() {
-        axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1&api_key=NCdfEq8UQhkl3dIummtSZ847JhDPU8N8ugFrgUSk').then(res => {
+        axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1&api_key=${API_KEY}`).then(res => {
             const data = res.data.photos;
             const daily = Object.keys(data).map(index => data[index].img_src);
             setDaily(daily);
